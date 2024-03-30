afc199e3c952f96b55bf55755c0ed1bd00791416
jose1805 <jvillalobos@flising.com>
2024-03-29 21:00:27 -0600

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Header, Guitar, Footer } from './components'
+import { db } from './data/db'
 import './App.css'
+import { useEffect, useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const initialCart = () => {
+		const localStorageCart = localStorage.getItem('cart')
+		return localStorageCart ? JSON.parse(localStorageCart) : [];
+	}
+	const [data, setData] = useState(db);
+	const [cart, setCart] = useState(initialCart)
+
+	const MAX_ITEMS = 5
+	const MIN_ITEMS = 0
+
+	useEffect( ()=>{
+		localStorage.setItem('cart', JSON.stringify(cart ?? '[]') )
+	},[cart])
+
+	const addToCart = (item) => {
+		const itemExist = cart.findIndex( (guitar) => guitar.id === item.id );
+		if( itemExist >= 0) {
+			if(cart[itemExist].quantity >= MAX_ITEMS) return;
+			const updateCart = [...cart];
+			updateCart[itemExist].quantity++
+			setCart(updateCart)
+		} else {
+			item.quantity = 1
+			setCart([...cart, item])
+		}
+	}
+
+	const removeFromCart = (id) => {
+		setCart( prevCart => (prevCart.filter( item => item.id !== id )) )
+	}
+
+
+	const increseQuantity = (id) => {
+		const updatedCart = cart.map( item => {
+			if( item.id === id && item.quantity < MAX_ITEMS){
+				return {
+					...item,
+					quantity: ++item.quantity
+				}
+			}
+				return item;
+		})
+		setCart(updatedCart)
+	}
+
+	const decreseQuantity = (id) => {
+		const updatedCart = cart.map( item => {
+			if( item.id === id && item.quantity > MIN_ITEMS){
+				return {
+					...item,
+					quantity: --item.quantity
+				}
+			}
+				return item;
+		})
+		setCart(updatedCart)
+
+	}
+
+
+	const clearCart = () => {
+		setCart([])
+	}
+
+	return (
+		<>
+			<Header cart={cart} clearCart={clearCart} removeFromCart={removeFromCart} increseQuantity={increseQuantity} decreseQuantity={decreseQuantity}/>
+
+			<main className="container-xl mt-5">
+				<h2 className="text-center">Nuestra Colección</h2>
+
+				<div className="row mt-5">
+					{
+						data.map( (guitar) => {
+							return <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart}/>
+						})
+					}
+				</div>
+
+			</main>
+
+			<Footer/>
+		
+		</>
+	)
 }
 
 export default App
